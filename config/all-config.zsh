b646b0c25b1c5a11f550a97539ff5e3459f0d918
carljohan <carljohan@ohmy.se>
2022-12-08 19:30:59 -0500

    fix: import local configs

--- config/all-config.zsh
+++ config/all-config.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo 'Config loaded from dotfiles'
+# Time the stuff.
+integer t0=$(date '+%s')
+
 # Fig pre block. Keep at the top of this file.
 [[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && . "$HOME/.fig/shell/zshrc.pre.zsh"
 
@@ -41,11 +45,11 @@ antigen apply
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
-# Example aliases
 alias zconf="code ~/.dotfiles/config/"
-#open current dir in vscode
-alias c="code ."
+
+alias ll="ls -a" # List all files including dotfiles
+
+alias c="code ." #open current dir in vscode
 
 # Fixing macOS zsh Terminal History Settings - https://blog.akatz.org/fixing-macos-zsh-terminal-history-settings/
 alias history="history 1"
@@ -54,11 +58,33 @@ HISTFILESIZE=99999
 SAVEHIST=$HISTSIZE
 setopt SHARE_HISTORY HIST_IGNORE_DUPS
 
-# for Brew
-export PATH="/usr/local/sbin:$PATH"
+# Set path to bin for git Brew
+#export PATH="/usr/local/bin:${PATH}"
+
+# GULP Path #
+export PATH=$HOME/.node/bin:$PATH
 
 # for gnupg
 export GPG_TTY=$(tty)
+
+# PNPM
+export PNPM_HOME="/Users/jopplas/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+# Place .zcompdump in zsh cache folder as per - https://stackoverflow.com/questions/62931101/i-have-multiple-files-of-zcompdump-why-do-i-have-multiple-files-of-these
+#export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
+
+
+# End of zshrc  (load time measure - https://stackoverflow.com/questions/4351244/can-i-profile-my-zshrc-zshenv/30791711#30791711)
+function {
+    local -i t1 startup
+    t1=$(date '+%s')
+    startup=$(( t1 - t0 ))
+    [[ $startup -gt 1 ]] && print "Hmm, poor shell startup time: $startup"
+    ##print "startup time: $startup"
+
+}
+unset t0
 
 
 # Fig post block. Keep at the bottom of this file.
